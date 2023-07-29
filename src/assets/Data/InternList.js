2dd44c77f96ca74de5f5318d41f6cb9dbb3c8dfb
Amit <amit7497925101@gmail.com>
2023-07-29 18:15:43 +0530

    updated

--- src/assets/Data/InternList.js
+++ src/assets/Data/InternList.js
@@ -1,6 +1,20 @@
 
 import JMDOFFER from '../Image/Intern/JMDOFFER.PNG'
+import PINNACLEWORKS from '../Image/Intern/Pinnacleworks.png'
 export const Internlist = [
+  {
+    company: "Pinnacleworks Infotech (p) ltd",
+    dec: `PinnacleWorks is a global provider of AI-Powered SaaS Based Products including SuperBot which empower businesses to move towards Smart Automation. The company has a renowned presence in the IT market with a large pool of clients from different sectors.`,
+    img: PINNACLEWORKS,
+    certi: "https://drive.google.com/file/d/1fAgsyQBWCkyyDCLcPmox_6if-3e0l_zb/view?usp=sharing",
+    tech: ' NODE | EXPRESS | MONGODB | SWAGGER | RABBITMQ | REDIS | PUBSUB | SOCKET.IO',
+    date: "01/July/2023 - Present",
+    role: "Software Developer",
+    time: "3 Months",
+    cmpURL: "https://pinnacle.works/"
+
+
+  },
   {
     company: "JMD Web Studio",
     dec: `This is to certify that Mr. Amit Verma has successfully completed his internship on 6th
@@ -20,8 +34,8 @@ a few sprints successfully. `,
     cmpURL: "https://jmdwebstudio.in/"
 
 
-  },
-  
+  }
+
 
 
 
