948e8791292e96815fb9c9931f1dec139b588a9f
amit5511 <amit7497925101@gmail.com>
2023-07-29 18:28:50 +0530

    updated

--- src/assets/Data/InternList.js
+++ src/assets/Data/InternList.js
@@ -6,7 +6,7 @@ export const Internlist = [
     company: "Pinnacleworks Infotech (p) ltd",
     dec: `PinnacleWorks is a global provider of AI-Powered SaaS Based Products including SuperBot which empower businesses to move towards Smart Automation. The company has a renowned presence in the IT market with a large pool of clients from different sectors.`,
     img: PINNACLEWORKS,
-    certi: "https://drive.google.com/file/d/1fAgsyQBWCkyyDCLcPmox_6if-3e0l_zb/view?usp=sharing",
+    certi: "https://drive.google.com/file/d/1onhJhKzqR_tQYpg3PTpTCIZeTTpUr_Dv/view?usp=sharing",
     tech: ' NODE | EXPRESS | MONGODB | SWAGGER | RABBITMQ | REDIS | PUBSUB | SOCKET.IO',
     date: "01/July/2023 - Present",
     role: "Software Developer",
